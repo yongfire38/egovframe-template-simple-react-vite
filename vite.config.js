8848175588e6d996cb22d51b64a1013fc653fd5f
yongfire38 <yongfire38@gmail.com>
2023-05-04 18:06:41 +0900

    dist 삭제

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/egovframe-template-simple-react-vite/',
   server: {
     port: 3000,
   },
