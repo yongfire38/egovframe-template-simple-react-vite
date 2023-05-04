0d2cbd380c7b86df9474cde357d4367f0279f720
yongfire38 <yongfire38@gmail.com>
2023-05-04 17:46:31 +0900

    config 수정

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:{
+    url: '/egovframe-template-simple-react-vite/'
+  },
   server: {
     port: 3000,
   },
