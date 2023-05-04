0b5e1a120a4ee55b328d6e6374da3e427639cc2f
yongfire38 <yongfire38@gmail.com>
2023-05-04 17:52:51 +0900

    dist 폴더 push

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:{
-    url: '/egovframe-template-simple-react-vite/'
-  },
+  base: '/egovframe-template-simple-react-vite/',
   server: {
     port: 3000,
   },
