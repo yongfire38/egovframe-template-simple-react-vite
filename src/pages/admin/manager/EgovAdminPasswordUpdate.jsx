4505298ca2b914fa60ce9db2da2498feffffa5fd
yongfire38 <yongfire38@gmail.com>
2024-08-02 10:11:32 +0900

    Fix: 비밀번호 변경 API를 Patch로 수정

--- src/pages/admin/manager/EgovAdminPasswordUpdate.jsx
+++ src/pages/admin/manager/EgovAdminPasswordUpdate.jsx
@@ -36,7 +36,7 @@ function EgovAdminPasswordUpdate(props) {
   };
 
   const updateAdminPassword = () => {
-    const editURL = "/adminPassword";
+    const editURL = "/admin/password";
 
     let requestOptions = {};
     const formData = new FormData();
@@ -44,7 +44,7 @@ function EgovAdminPasswordUpdate(props) {
     formData.append("new_password", newPassword);
     if (formValidator(formData)) {
       requestOptions = {
-        method: "POST",
+        method: "PATCH",
         headers: {
           "Content-type": "application/json",
         },
